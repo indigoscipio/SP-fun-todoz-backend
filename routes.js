3c76eea810668f8b5cdd5cbddbf571156a0aefc0
Samuel Oktavianus <samueloctavianus97@gmail.com>
2023-10-25 20:57:56 +0700

    test

--- routes.js
+++ routes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const Task = require("./models/Task");
 
-// Create a new task
+// Create a new tasks
 router.post("/tasks", async (req, res) => {
   try {
     const task = new Task(req.body);
